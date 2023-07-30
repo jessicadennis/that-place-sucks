ddff0d39181bdd604b93cc1a457dc19ade87eb89
Jessica Dennis <jessicadennis@gmail.com>
2023-07-30 14:56:16 -0400

    Routing works

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./index.css";
 import App from "./App";
-import React from "react";
+import ErrorPage from "./error-page";
 import ReactDOM from "react-dom/client";
 import awsExports from "./aws-exports";
 import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
 import { AmplifyProvider } from "@aws-amplify/ui-react";
+import { React, StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,16 +16,38 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 // FontAwesome
 import "@fortawesome/fontawesome-free/css/fontawesome.css";
 import "@fortawesome/fontawesome-free/css/solid.css";
+import PlaceForm from "./PlaceForm";
+import PlaceList from "./components/PlaceList";
 
 Amplify.configure(awsExports);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <PlaceList />,
+      },
+      {
+        path: "add",
+        element: <PlaceForm />,
+      },
+    ],
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-document.getElementById("root").setAttribute("data-bs-theme", "dark");
+document.querySelector("html").setAttribute("data-bs-theme", "dark");
 
 root.render(
-  <AmplifyProvider>
-    <App />
-  </AmplifyProvider>
+  <StrictMode>
+    <AmplifyProvider>
+      <RouterProvider router={router} />
+    </AmplifyProvider>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
