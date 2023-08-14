9ed57cc73eb25800e897b9d86c06eead64b04004
Jessica Dennis <jessicadennis@gmail.com>
2023-08-14 09:41:45 -0400

    Copy queries from queries.js to custom-queries
    * For some reason it just refuses to import them from the queries.js, and for some equally mysterious reason, queries.ts is missing these.

--- src/graphql/custom-queries.ts
+++ src/graphql/custom-queries.ts
@@ -1,5 +1,5 @@
-export const getAllRestaurants = `
-  query getAllRestaurants {
+export const getAllRestaurants = /* GraphQL */ `
+  query getAllRestaurants() {
     listRestaurants {
       items {
         categories {
@@ -25,11 +25,12 @@ export const getAllRestaurants = `
         updatedAt
       }
     }
-  }`;
+  }
+`;
 
-export const listValidCategories = `
+export const listValidCategories = /* GraphQL */ `
   query listValidCategories {
-    listCategories(limit: 1000, filter: {_deleted: {ne: true}}) {
+    listCategories(limit: 1000, filter: { _deleted: { ne: true } }) {
       items {
         name
         id
@@ -39,7 +40,7 @@ export const listValidCategories = `
   }
 `;
 
-export const getRestaurantById = `
+export const getRestaurantById = /* GraphQL */ `
   query GetRestaurantById($id: ID!) {
     getRestaurant(id: $id) {
       categories {
@@ -62,6 +63,205 @@ export const getRestaurantById = `
           updatedAt
         }
       }
+    }
+  }
+`;
+
+// These are literally copied and pasted from queries.js but whatever, amplify
+export const getRestaurantCategory = /* GraphQL */ `
+  query GetRestaurantCategory($id: ID!) {
+    getRestaurantCategory(id: $id) {
+      id
+      restaurantId
+      categoryId
+      restaurant {
+        id
+        name
+        rating
+        createdAt
+        updatedAt
+        __typename
+      }
+      category {
+        id
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listRestaurantCategories = /* GraphQL */ `
+  query ListRestaurantCategories(
+    $filter: ModelRestaurantCategoryFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listRestaurantCategories(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        restaurantId
+        categoryId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const restaurantCategoriesByRestaurantId = /* GraphQL */ `
+  query RestaurantCategoriesByRestaurantId(
+    $restaurantId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelRestaurantCategoryFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    restaurantCategoriesByRestaurantId(
+      restaurantId: $restaurantId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        restaurantId
+        categoryId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const restaurantCategoriesByCategoryId = /* GraphQL */ `
+  query RestaurantCategoriesByCategoryId(
+    $categoryId: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelRestaurantCategoryFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    restaurantCategoriesByCategoryId(
+      categoryId: $categoryId
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        restaurantId
+        categoryId
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const createRestaurantCategory = /* GraphQL */ `
+  mutation CreateRestaurantCategory(
+    $input: CreateRestaurantCategoryInput!
+    $condition: ModelRestaurantCategoryConditionInput
+  ) {
+    createRestaurantCategory(input: $input, condition: $condition) {
+      id
+      restaurantId
+      categoryId
+      restaurant {
+        id
+        name
+        rating
+        createdAt
+        updatedAt
+        __typename
+      }
+      category {
+        id
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateRestaurantCategory = /* GraphQL */ `
+  mutation UpdateRestaurantCategory(
+    $input: UpdateRestaurantCategoryInput!
+    $condition: ModelRestaurantCategoryConditionInput
+  ) {
+    updateRestaurantCategory(input: $input, condition: $condition) {
+      id
+      restaurantId
+      categoryId
+      restaurant {
+        id
+        name
+        rating
+        createdAt
+        updatedAt
+        __typename
+      }
+      category {
+        id
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteRestaurantCategory = /* GraphQL */ `
+  mutation DeleteRestaurantCategory(
+    $input: DeleteRestaurantCategoryInput!
+    $condition: ModelRestaurantCategoryConditionInput
+  ) {
+    deleteRestaurantCategory(input: $input, condition: $condition) {
+      id
+      restaurantId
+      categoryId
+      restaurant {
+        id
+        name
+        rating
+        createdAt
+        updatedAt
+        __typename
+      }
+      category {
+        id
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      createdAt
+      updatedAt
+      __typename
     }
   }
 `;
