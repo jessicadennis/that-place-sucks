3cc9400d29a9c8e94d9e391078c7df5da4ba037b
Jessica Dennis <jessicadennis@gmail.com>
2023-09-05 15:42:03 -0400

    Offcanvas nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import { faHamburger, faUser } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useQueryClient } from "react-query";
 import { NavLink, useNavigate } from "react-router-dom";
 
 export default function NavBar() {
@@ -8,6 +9,7 @@ export default function NavBar() {
     context.route,
     context.signOut,
   ]);
+  const queryClient = useQueryClient();
 
   const navigate = useNavigate();
 
@@ -26,6 +28,7 @@ export default function NavBar() {
 
   function logOut() {
     signOut();
+    queryClient.invalidateQueries();
     navigate("/");
   }
 
@@ -42,66 +45,75 @@ export default function NavBar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-bs-toggle="collapse"
           data-bs-target="#navMenu"
           aria-controls="navMenu"
-          aria-expanded="false"
+          data-bs-toggle="offcanvas"
           aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faHamburger} />
         </button>
         <div
-          className="collapse navbar-collapse"
+          className="offcanvas offcanvas-end"
+          tabIndex={-1}
           id="navMenu">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item pb-2 border-bottom border-secondary">
-              <div className="d-flex align-items-center">
-                <FontAwesomeIcon icon={faUser} />
-                {route !== "authenticated" ? (
+          <div className="offcanvas-header">
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item pb-2 border-bottom border-secondary">
+                <div className="d-flex align-items-center">
+                  <FontAwesomeIcon icon={faUser} />
+                  {route !== "authenticated" ? (
+                    <NavLink
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "nav-link ms-3 active disabled"
+                          : isPending
+                          ? "pending"
+                          : "nav-link ms-3"
+                      }
+                      to={"/login"}>
+                      Log in
+                    </NavLink>
+                  ) : (
+                    <>
+                      <span className="ms-2">
+                        {user?.attributes?.given_name ?? ""}{" "}
+                        {user?.attributes?.family_name ?? ""}
+                      </span>
+                      <button
+                        className="btn btn-sm btn-outline-light ms-3"
+                        type="button"
+                        onClick={logOut}>
+                        Log Out
+                      </button>
+                    </>
+                  )}
+                </div>
+              </li>
+              {pages.map((page) => (
+                <li
+                  key={page.id}
+                  className="nav-item">
                   <NavLink
+                    to={page.route}
                     className={({ isActive, isPending }) =>
                       isActive
-                        ? "nav-link ms-3 active disabled"
+                        ? "nav-link active disabled"
                         : isPending
                         ? "pending"
-                        : "nav-link ms-3"
-                    }
-                    to={"/login"}>
-                    Log in
+                        : "nav-link"
+                    }>
+                    {page.name}
                   </NavLink>
-                ) : (
-                  <>
-                    <span className="ms-2">
-                      {user?.attributes?.given_name ?? ""}{" "}
-                      {user?.attributes?.family_name ?? ""}
-                    </span>
-                    <button
-                      className="btn btn-sm btn-outline-light ms-3"
-                      type="button"
-                      onClick={logOut}>
-                      Log Out
-                    </button>
-                  </>
-                )}
-              </div>
-            </li>
-            {pages.map((page) => (
-              <li
-                key={page.id}
-                className="nav-item">
-                <NavLink
-                  to={page.route}
-                  className={({ isActive, isPending }) =>
-                    isActive
-                      ? "nav-link active disabled"
-                      : isPending
-                      ? "pending"
-                      : "nav-link"
-                  }>
-                  {page.name}
-                </NavLink>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </nav>
